d5acfd617a2f2b2d4b5c54f882089f2ba1715242
Thomason, James <JamesThomason@QuickenLoans.com>
2018-07-21 20:35:08 -0400

    Minor configuration and layout changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,22 @@
 module.exports = {
+  srcDir: 'src/',
+  mode: 'spa',
+  modules: [
+    '@nuxtjs/bulma',
+    '@nuxtjs/markdownit',
+    '@nuxtjs/blog',
+    'nuxt-typescript',
+    'qonfucius-nuxt-fontawesome'
+  ],
+  css: [
+    '~~/assets/css/main.scss'
+  ],
+  fontAwesome: {
+    packs: [{
+      package: '@fortawesome/fontawesome-free-brands',
+      icons: ["faFacebookSquare", "faGooglePlusSquare", "faTwitterSquare"]
+      }]
+  },
   generate: {
     dir: 'docs'
   }
